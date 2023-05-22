f1477c71b8e3d4f6272bfd1519c406b61f1e71df
leonfernandes <44719892+leonfernandes@users.noreply.github.com>
2023-05-22 14:08:42 +0530

    references and index

--- R/adcf-package.R
+++ R/adcf-package.R
@@ -1,3 +1,20 @@
+#' @references Phyllis Wan, Richard A. Davis, Goodness-of-fit testing for time
+#'      series models via distance covariance, Journal of Econometrics,
+#'      Volume 227, Issue 1, 2022, Pages 4-24, ISSN 0304-4076,
+#'      https://doi.org/10.1016/j.jeconom.2020.05.008.
+#' @references Richard A. Davis, Muneya Matsui, Thomas Mikosch, Phyllis Wan
+#'      "Applications of distance correlation to time series," Bernoulli,
+#'      Bernoulli 24(4A), 3087-3116, (November 2018).
+#' @references Xiaoming Huo & Gábor J. Székely (2016) Fast Computing for
+#'      Distance Covariance, Technometrics, 58:4, 435-447,
+#'      DOI: 10.1080/00401706.2015.1054435.
+#' @references Gábor J. Székely, Maria L. Rizzo, Nail K. Bakirov
+#'      "Measuring and testing dependence by correlation of distances," The
+#'      Annals of Statistics, Ann. Statist. 35(6), 2769-2794, (December 2007).
+#' @aliases {adcf}-package
+#' @keywords internal
+"_PACKAGE"
+
 ## usethis namespace: start
 #' @useDynLib adcf, .registration = TRUE
 ## usethis namespace: end
